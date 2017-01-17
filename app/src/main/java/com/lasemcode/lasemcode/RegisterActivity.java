65aca76834857e05588f4857cbd20bfd6824e302
Lasem Code <miftakhul.miftakhulhudha@gmail.com>
2017-01-18 01:14:30 +0700

    firt commit

---
+++ app/src/main/java/com/lasemcode/lasemcode/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.lasemcode.lasemcode;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lasemcode on 17/01/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
